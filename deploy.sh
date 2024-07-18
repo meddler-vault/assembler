68dff5198963e38b4aacaa6e3bcb47da34a8dbe6
Your Name <you@example.com>
2024-07-19 00:06:52 +0530

    remove

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-# docker build --build-arg GOOS=linux --build-arg GOARCH=amd64  -f deploy/Dockerfile . -t rounak316/kaniko_test:nightly
+docker build -f deploy/Dockerfile . -t rounak316/kaniko_test:nightly
 # docker buildx build --platform linux/amd64  -f deploy/Dockerfile  -t rounak316/kaniko_test:nightly
 # For linux
-docker build --build-arg TARGETOS=linux --build-arg TARGETARCH=amd64  -f deploy/Dockerfile . -t rounak316/kaniko_test:nightly
+# docker build --build-arg TARGETOS=linux --build-arg TARGETARCH=amd64  -f deploy/Dockerfile . -t rounak316/kaniko_test:nightly
 
 docker push rounak316/kaniko_test:nightly
 
